3a6d35faa255f7fd5146b7933bf1ed900070e076
JustinWang23 <731614391@qq.com>
2017-12-04 17:43:50 +0800

    add upload modules

--- routes/tests.js
+++ routes/tests.js
@@ -1,6 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var fs = require('fs');
+var multer  = require('multer')
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, "../uploads/"))
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+var limits = {
+	fields: 10,
+	fileSize: 5*1024*1024,
+	files: 5
+}
+
+function fileFilter (req, file, cb) {
+
+  if (file.originalname.indexOf('.jpg') > -1) {
+  	// 拒绝这个文件，使用`false`，像这样:
+  	cb(null, false)
+  	// 如果有问题，你可以总是这样发送一个错误:
+  	cb(new Error('No JPG File!'))
+  } else {
+  	// 接受这个文件，使用`true`，像这样:
+  	cb(null, true)
+  }
+
+}
+
+var upload = multer({ storage: storage, limits: limits, fileFilter: fileFilter })
 
 // 引入模块
 var ModelProxy = require( 'modelproxy-copy' );
@@ -27,14 +60,23 @@ router.get("/getFile/:filename", function (req, res, next) {
 	} )
 });
 
+router.post("/uploadFile", upload.single('thumbnail'), function (req, res, next) {
+
+	res.send('File uploaded to: ' + req.file.path + ' - ' + req.file.size + ' bytes');
+});
+
+router.get("/test.html", function (req, res, next) {
+	res.render('test');
+});
+
 router.get("/:user/:gender", function (req, res, next) {
 	var params = req.params;
 
 	// 合并请求
 	model.get( { gender: params.gender } )
-		.post( { name: params.user} )
+		.post( { name: params.user } )
 	    .done( function( data1, data2 ) {
-	        res.status(200).json({data1: data1,data2: data2})
+	        res.status(200).json( { data1: data1, data2: data2 } )
 	    } )
 	    .error( function( err ) {
         	console.log( err );
